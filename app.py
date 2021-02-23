b0b5c52a48bd49552b807a7d73cf1e90ac4aef74
Rafael <7544832+rafaelsgirao@users.noreply.github.com>
2021-02-23 23:48:53 +0000

    Fix things so it works now

--- app.py
+++ app.py
@@ -1,16 +1,56 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_wtf import Form
-from wtforms import TextField, BooleanField
+import re
+from requests import post
+from flask import Flask, render_template, request, redirect, url_for, escape
+# from flask_wtf import Form
+# from wtforms import TextField, BooleanField
 from uuid import uuid4
 import json
+
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'DaRshLqsK7TVjgUUXHoQSRnmMfQn6orDeAxjLEaGPdGqF2wdKwuCkzpdwXvQdadX'
+import sys
+EMAIL_EXPR = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+f = open("courses_by_degree.json", "r", encoding="utf-8")
+#Load Courses By Degree
+cbd = json.load(f)
+f.close()
+
+# Todo: Regen and load this from .env file
+app.config['SECRET_KEY']=getenv('FLASK_SECRET_KEY')
+
+def send_confirmation_email(email, link):
+    return post(
+        f"https://api.eu.mailgun.net/v3/{getenv('MAILGUN_DOMAIN')}/messages",
+        auth=("api", getenv("MAILGUN_API_KEY")),
+
+        data={"from": f"Fónix Anúncios <fonix-notifications@{getenv('MAILGUN_DOMAIN')}>",
+              "to": [email],
+              "subject": "Fónix Anúncios | Confirma o teu e-mail",
+              "text": f"Olá! Carrega no link abaixo para confirmares o teu e-mail.\n{link} \n Se não foste tu a pedir este e-mail podes ignorá lo."}).text
+
+def add_list_member(email, mailing_lists):
+    responses = []
+    for mailing_list in mailing_lists:
+        responses.append(post(
+            f"https://api.eu.mailgun.net/v3/lists/{mailing_list}/members.json",
+            auth=("api", getenv("MAILGUN_API_KEY")),
+            data={'upsert': True,
+                'members': f'[{{"address": "<{email}>"}}]'}).text)
+    return responses
+
+    
+def check_email(email):
+    global EMAIL_EXPR
+    return bool(re.search(EMAIL_EXPR, email))
 
 def load_cfg():
     global cfg
     with open("conf.json", "r+", encoding="utf-8") as f:
-        cfg = json.load(f)
+        cfg=json.load(f)
 
 def dump_cfg():
     global cfg
@@ -21,57 +61,94 @@ def dump_cfg():
 
     print("Dumped to file 'dirs.json'")
 
-@app.route('/')
+@ app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/register-done')
+@ app.route('/register-done')
 def register_done():
     return render_template('register-done.html')
 
-@app.route('/confirm/<token>')
+@ app.route('/confirm/<token>')
 def confirm(token):
     global cfg
+    global cbd
     token_found=False
-    print(cfg.items())
-    for email_cfg, token_cfg in cfg.items():
-        if token == token_cfg:
-            print("yes")
+    for email in cfg:
+        if cfg[email]["token"] == token:
             token_found=True
-            cfg.pop(email_cfg)
-            dump_cfg()
-            header = "Obrigado por confirmares!"
-            text = f"O teu e-mail {email_cfg} confirmado. Já deves começar a receber os próximos anúncios neste e-mail."
-            return render_template('info.html', text=text, header=header)
-    if not token_found:
 
-        text = 'ERRO: Token Inválido.'
-        return render_template('info.html', text=text)
+            add_list_member(email, cfg[email]["subscriptions"])
+            cfg.pop(email)
+            dump_cfg()
+            header="Obrigado por confirmares!"
+            text=f"O teu e-mail {email} confirmado. Já deves começar a receber os próximos anúncios."
+            return render_template('confirm.html', text=text, header=header)
+    if not token_found:
+        header = "Erro"
+        text='Token Inválido (provavelmente já validaste o teu e-mail?).'
+        return render_template('confirm.html', text=text, header=header)
 
 # Simple form handling using raw HTML forms
-@app.route('/register', methods=['GET', 'POST'])
-def sign_up():
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
     global cfg
-    error = ""
+    text = ""
     if request.method == 'POST':
         # Form being submitted; grab data from form.
-        #leic_a = request.form.getlist('leic-a')
-        data = request.form.to_dict()
-        #last_name = request.form['lastname']
-        print(data)
+        email=escape(request.form['email'])
+
+
+        form_data_correct = True
+        #loop through every course in courses by degree
+        #pretty sure there's a better way to do this
+        subscriptions = []
+        for course in cbd:
+            selected_degrees = request.form.getlist(course)
+            if len(selected_degrees) == 0:
+                continue 
+            for degree in selected_degrees:
+                if degree not in cbd[course]:
+                    text = f"ERRO: Cadeira '{degree}'' inválida em '{course}''."
+                    form_data_correct = False
+                    break
+                elif cbd[course][degree] == "":
+                    form_data_correct = False
+                    text = f"ERRO: Cadeira '{degree}'' inválida em '{course}''."
+                    break
+                else:
+                    subscriptions.append(f"{course.lower()}-{degree.lower()}-anuncios@{getenv('MAILGUN_DOMAIN')}")
+        print(subscriptions)
+        print(len(subscriptions))
+        if len(subscriptions) == 0:
+            text = "ERRO: Tens que selecionar alguma coisa!"
+            form_data_correct = False
+        form_data_correct = True
+
         # Validate form data
-        if len(data) == 0:
-            # Form data failed validation; try again
-            error = "ERRO: Tens que selecionar alguma coisa"
+        if not check_email(email):
+            text = "ERRO: Email inválido."
+        elif not form_data_correct:
+            text = "ERRO: Disciplinas e/ou curso inválidas."
         else:
             # Form data is valid; move along
-            cfg[data["email"]] = str(uuid4())
+            token = str(uuid4())
+            #cfg[email]=token
+            cfg[email]= {
+                "token": token,
+                "subscriptions": subscriptions
+            }
             dump_cfg()
+            #Get current link without request path, e.g http://localhost:5000
+            base_link = request.base_url[:-len(request.path)]
+            
+            link = base_link + url_for("confirm", token=token)
+            print(send_confirmation_email(email, link))
             return redirect(url_for('register_done'))
 
     # Render the sign-up page
-    return render_template('sign-up.html', message=error)
+    return render_template('register.html', message=text)
 
 
-load_cfg()    
+load_cfg()
 app.run(debug=True)
--- make_mailing_lists.py
+++ make_mailing_lists.py
@@ -1,9 +1,16 @@
 import json
 import requests
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from os import getenv
+
 f = open("courses_by_degree.json", "r", encoding="utf-8")
 data = json.load(f)
 f.close()
 #print(data)
+
 def create_mailing_list(course, uc):
     address = f'{course.lower()}-{uc.lower()}-anuncios@mg.rafael.ovh'
     description = f"Receber anúncios de {uc} para {course}"
@@ -11,22 +18,29 @@ def create_mailing_list(course, uc):
     print(description)
     return requests.post(
         "https://api.eu.mailgun.net/v3/lists",
-        auth=('api', 'key-80b486670c968b16bfb2e1c7db703317'),
+        auth=('api', getenv('MAILGUN_API_KEY')),
         data={'address': address,
               'description': description,
               'reply_preference': 'sender'
         }).text
 
-template = """<p><input type=checkbox name={course} value='{uc}'> {uc_high} <br/></p>"""
+def make_template(data):
+    template = """<p><input type=checkbox name={course} value='{uc}'> {uc} <br/></p>"""
+
+    for course in data:
+        print("        <h2>{}</h2>".format(course))
+        print('        <button onclick="return false;" class="collapsible">Abrir </button>')
+        print('        <div class="content">')
+
+        for uc in data[course]:
+            if data[course][uc] != "":
+                #print(f"{uc} - {course} | Fónix Anúncios")
+                #print(create_mailing_list(course, uc))
+                print(template.format(course=course, uc=uc))
+        print('        </div>')
 
 for course in data:
-    print("        <h2>{}</h2>".format(course))
-    print('        <button onclick="return false;" class="collapsible">Abrir </button>')
-    print('        <div class="content">')
-
     for uc in data[course]:
         if data[course][uc] != "":
-            #print(f"{uc} - {course} | Fónix Anúncios")
+            print(f"{uc} - {course} | Fónix Anúncios")
             #print(create_mailing_list(course, uc))
-            print(template.format(course=course.lower(), course_high=course, uc=uc.lower(), uc_high=uc))
-    print('        </div>')
